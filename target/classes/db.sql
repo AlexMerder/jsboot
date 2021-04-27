766f3757f29529ff0be1f0d68d9eeb506e3d7ad3
Aleksey Merezhin <63518412+AlexMerder@users.noreply.github.com>
2021-04-27 14:51:46 +0300

    Revert "change db settings and add new rest controller for db"
    This reverts commit 9457f44fa503bd85dc02a2f3ccd799cfccb0ab07.

--- target/classes/db.sql
+++ target/classes/db.sql
@@ -1,16 +1,27 @@
--- Users
+# DROP TABLE IF EXISTS `users`;
+#
+# CREATE TABLE `users` (
+#                             `id` bigint NOT NULL AUTO_INCREMENT primary key ,
+#                             `first_name` varchar(45) DEFAULT NULL,
+#                             `last_name` varchar(45) DEFAULT NULL,
+#                             `email` varchar(45) DEFAULT NULL,`password` varchar(255) not null
+# ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `employee`
+--
+
 INSERT INTO users VALUES
-(1,12,'leslie@luv2code.com','Andrews','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e','Leslie'),
-(2,43,'emma@luv2code.com','Baumgarten','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e','Emma'),
-(3,33,'avani@luv2code.com','Gupta','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e','Avani'),
-(4,23,'yuri@luv2code.com','Petrov','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e','Yuri'),
-(5,42,'juan@luv2code.com','Vega','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e','Juan');
+(7,12,'leslie@luv2code.com','Andrews','Leslie','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e');
+# (2,43,'emma@luv2code.com','Baumgarten','Emma','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e'),
+# (3,33,'avani@luv2code.com','Gupta','Avani','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e'),
+# (4,23,'yuri@luv2code.com','Petrov','Yuri','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e'),
+# (5,42,'juan@luv2code.com','Vega','Juan','$2a$10$yTnrThU8bAirB2CIt7lFwekVcD.qNz5gWsGJfz9b26IwUfcHNTl5e');
+# insert into roles values (1,'ROLE_ADMIN');
+# insert into roles values (2,'ROLE_USER');
 
---Roles
-insert into roles values (1,'ROLE_ADMIN');
-insert into roles values (2,'ROLE_USER');
 
---Users_Roles
+
 insert into users_roles values (1,1);
 insert into users_roles values (1,2);
 insert into users_roles values (2,1);
